e4485caaf4d5359bf19552d06813b654101c6cc7
MatheusDer <matheusder1@hotmail.com>
2022-05-14 16:45:00 -0300

    criar a pagina de login sendo o index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,43 @@
+import Link from "next/link";
+import { Card, Button, Container, Form, Row, Col } from "react-bootstrap";
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col xl={6}>
+          <Card body className="p-4 shadow-lg">
+            <Card.Title>
+              <h1>Site APS</h1>
+            </Card.Title>
+            <Form className="mt-5">
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Endereco de Email</Form.Label>
+                <Form.Control type="email" placeholder="nome@email.com" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="senha">
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" placeholder="*******" />
+                <Form.Text className="text-muted">
+                  Voce nao deve compartilhar sua senha!
+                </Form.Text>
+              </Form.Group>
+              <div className="text-center">
+                <Button
+                  type="submit"
+                  variant="outline-dark"
+                  className="w-50 mt-4"
+                >
+                  Confirmar
+                </Button>
+              </div>
+            </Form>
+            <Card.Text className="mt-5">
+              Nao possui uma conta? <Link href="#">Cadastre-se</Link>
+            </Card.Text>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
