bf298ce755938e94300439f14f9f88496cb42a69
MatheusDer <matheusder1@hotmail.com>
2022-05-14 16:44:25 -0300

    adicionar item sobre e atualizar as rotas existentes

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -8,13 +8,16 @@ function NavbarLayout() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/posts" className="me-3">
+            <Nav.Link href="/posts" className="me-4">
               Home
             </Nav.Link>
-            <Nav.Link href="/usuario" className="me-3">
+            <Nav.Link href="/usuario" className="me-4">
               Profile
             </Nav.Link>
-            <Nav.Link href="/sobre" className="me-3">
+            <Nav.Link href="/criar-post" className="me-4">
+              Criar Post
+            </Nav.Link>
+            <Nav.Link href="/sobre" className="me-4">
               Sobre
             </Nav.Link>
           </Nav>
