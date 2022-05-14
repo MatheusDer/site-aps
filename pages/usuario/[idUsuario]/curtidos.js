8e293ae72884d6c477cd102d951d847170909dbd
MatheusDer <matheusder1@hotmail.com>
2022-05-14 09:53:39 -0300

    criar rotas do site

---
+++ pages/usuario/[idUsuario]/curtidos.js
@@ -0,0 +1,9 @@
+function LikedPostsPages() {
+  return (
+    <div>
+      <h1>Liked Posts Page</h1>
+    </div>
+  );
+}
+
+export default LikedPostsPages;
---
+++ pages/usuario/[idUsuario]/posts.js
@@ -0,0 +1,9 @@
+function UserPostsPage() {
+  return (
+    <div>
+      <h1>User Posts</h1>
+    </div>
+  );
+}
+
+export default UserPostsPage;
