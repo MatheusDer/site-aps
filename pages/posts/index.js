735b3702b0d762d505da844de748b7057575137c
MatheusDer <matheusder1@hotmail.com>
2022-05-20 10:49:29 -0300

    (/usuario/[id]) passar o id do usuario logado para a pagina de perfil

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,25 +3,21 @@ import NavbarLayout from "../../components/layout/navbar";
 import { useRouter } from "next/router";
 
 function PostsHomePage() {
-  const [usuario, setUsuario] = useState();
+  const [idUsuario, setIdUsuario] = useState();
   const router = useRouter();
   useEffect(() => {
     if (router.query.id == undefined) {
       router.push("/");
     }
-    fetch(`/api/usuario/${router.query.id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setUsuario(data.usuario);
-      });
+
+    setIdUsuario(router.query.id);
   }, []);
 
   return (
     <Fragment>
-      <NavbarLayout />
+      <NavbarLayout idUsuario={idUsuario} />
       <div>
         <h1>Posts</h1>
-        {usuario && usuario.apelido}
       </div>
     </Fragment>
   );
--- pages/usuario/[idUsuario]/index.js
+++ pages/usuario/[idUsuario]/index.js
@@ -1,7 +1,21 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 function ProfilePage() {
+  const [usuario, setUsuario] = useState();
+  const router = useRouter();
+
+  if (router.query.idUsuario != undefined) {
+    fetch(`/api/usuario/${router.query.idUsuario}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsuario(data.usuario);
+      });
+  }
+
   return (
     <div>
       <h1>Profile</h1>
+      {usuario && usuario.id}
     </div>
   );
 }
